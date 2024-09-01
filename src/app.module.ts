474ddfc4a9d47cf5d2c10070ab55a43a4fd1734e
sistracia <156994197+sistracia@users.noreply.github.com>
2024-09-01 17:09:19 +0700

    Add `.env.example`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
+import configuration from "./config/configuration";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env"],
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import {
 } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 
+const PORT = Number(process.env.PORT);
+const HOST = process.env.HOST;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(PORT, HOST);
 }
 bootstrap();
